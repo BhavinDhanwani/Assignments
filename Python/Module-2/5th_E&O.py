96d3bb52f1940bf3dc59b23ab34f1f1625153566
Bhavin Dhanwani <bhavindhanwani@gmail.com>
2023-10-10 15:17:49 +0530

    bhahd

---
+++ Python/Module-2/5th_E&O.py
@@ -0,0 +1,8 @@
+# Write a Python program to find whether a given number is even or odd, print out an appropriate message to the user.
+
+a=int(input("Enter First Number:"))
+
+if a%2==0:
+    print("The Firse Number is Even")
+else:
+    print("The Firse Number is Odd")
