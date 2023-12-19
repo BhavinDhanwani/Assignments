b26a5e9d362641a69aee995c11b941a670b0176a
Bhavin Dhanwani <bhavindhanwani@gmail.com>
2023-12-19 15:57:50 +0530

    python

--- Python Assignment/Module-4/4th_read_last_n_line.py
+++ Python Assignment/Module-4/4th_read_last_n_line.py
@@ -1,5 +1,7 @@
 # Write a Python program to read last n lines of a file.
 
 f=open("python.txt","r")
-for i in f:
-    print(f.readline()[-1:])
+n=int(input("Enter number of lines you want to read from last line : "))
+reverse=f.readlines()
+for i in range(1,n+1):
+    print(reverse[-1])
